b98ce32f5e94ae7807fb42fcf18c4971a075a168
corradosantoro <santoro@dmi.unict.it>
2018-03-19 11:36:45 +0100

    added

--- python/arm_response.py
+++ python/arm_response.py
@@ -5,12 +5,12 @@ import pylab
 from systems import *
 from inputs import *
 
-delta_t = 0.01 # 10ms di tempo di campionamento
-durata_simulazione = 120 # secondi
+delta_t = 0.001 # 1ms di tempo di campionamento
+durata_simulazione = 15 # secondi
 
 numero_punti = int(durata_simulazione / delta_t)
 
-the_robot = Robot(5, 0.6)
+the_arm = RoboticArm(6, 5.0)
 
 costant = Step(10)
 impulse = Impulse(10)
@@ -20,11 +20,11 @@ speed = []
 position = []
 for k in range(0, numero_punti):
     input_signal = costant.evaluate(delta_t)
-    the_robot.evaluate(input_signal, delta_t)
+    the_arm.evaluate(input_signal, delta_t)
 
     times.append(k * delta_t)
-    speed.append(the_robot.get_speed())
-    position.append(the_robot.get_pos())
+    speed.append(the_arm.get_omega())
+    position.append(the_arm.get_theta())
 
 pylab.figure(1)
 pylab.plot(times, speed, 'b-+')
@@ -37,14 +37,14 @@ pylab.title("Posizione - Ingresso Costante")
 times = []
 speed = []
 position = []
-the_robot.reset()
+the_arm.reset()
 for k in range(0, numero_punti):
     input_signal = impulse.evaluate(delta_t)
-    the_robot.evaluate(input_signal, delta_t)
+    the_arm.evaluate(input_signal, delta_t)
 
     times.append(k * delta_t)
-    speed.append(the_robot.get_speed())
-    position.append(the_robot.get_pos())
+    speed.append(the_arm.get_omega())
+    position.append(the_arm.get_theta())
 
 pylab.figure(3)
 pylab.plot(times, speed, 'r-+')
